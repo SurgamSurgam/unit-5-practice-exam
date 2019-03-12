fe6bb938d2295bb7302a74d4dd5e6f44451c77ad
Deyvi <deyvio@gmail.com>
2019-03-12 15:15:39 -0400

    spinned express app + made sql table

---
+++ backend/db/movies_final.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS movies_final;
+CREATE DATABASE movies_final;
+
+\c movies_final;
+
+
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL UNIQUE,
+  genre_id INT REFERENCES genres(id) ON DELETE CASCADE,
+  img_url TEXT
+);
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY,
+  stars numeric CHECK(stars >= 1 AND stars <= 5) NOT NULL,
+  movie_id INT REFERENCES movies(id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  body TEXT NOT NULL,
+  movie_id INT REFERENCES movies(id) ON DELETE CASCADE
+);
